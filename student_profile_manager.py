34767681d9245bf45b3ca459a169733795de55e9
Tarun Chaudhary ( The Silent Ghost ) <iamtarunchaudhary.dev@gmail.com>
2025-12-24 23:13:29 +0530

    Refactor Student class and add Online/Offline subclasses
    Added support for student class differentiation with OnlineStudent and OfflineStudent subclasses. Enhanced Student class to include student_class attribute.

--- student_profile_manager.py
+++ student_profile_manager.py
@@ -1,17 +1,30 @@
 class Student:
-    def __init__(self,name,roll_number,course):
+    def __init__(self,name,roll_number,course,student_class):
         self.name = name
         self.roll_number = roll_number
         self.course = course
+        self.student_class = student_class
     
     def display_info(self):
         print(self.name)   
         print(self.roll_number)   
         print(self.course)  
+        print(self.student_class)
         
-s1 = Student("Tarun", 7 , "Ethical Hacking")
-s2 = Student("Ritu", 28 , "Full Stack")
+class OnlineStudent(Student):
+    def online_info(self):
+        self.display_info()
+        print(f"{self.name} in online Program")
 
-s1.display_info()
+class OfflineStudent(Student):
+    def offline_info(self):
+        self.display_info()
+        print(f"{self.name} in offline Program")
+
+        
+s1 = OnlineStudent("Tarun", 7 , "Ethical Hacking","10th")
+s2 = OfflineStudent("Ritu", 28 , "Full Stack","12th")
+
+s1.online_info()
 print("-" * 20)
-s2.display_info()
+s2.offline_info()
