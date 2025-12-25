26eaf432a007d058fd6fcbe47f3d1401d6e46c43
Tarun Chaudhary ( The Silent Ghost ) <iamtarunchaudhary.dev@gmail.com>
2025-12-25 23:42:21 +0530

    Refactor Student class to use abstract methods
    Refactor Student class to be abstract and implement study_mode method in subclasses.

--- student_profile_manager.py
+++ student_profile_manager.py
@@ -1,9 +1,15 @@
-class Student:
+from abc import ABC, abstractmethod
+
+class Student(ABC):
     def __init__(self,name,roll_number,course,student_class):
         self.name = name
         self.roll_number = roll_number
         self.course = course
         self.student_class = student_class
+    
+    @abstractmethod
+    def study_mode(self):
+        pass    
     
     def display_info(self):
         print(self.name)   
@@ -14,17 +20,24 @@ class Student:
 class OnlineStudent(Student):
     def online_info(self):
         self.display_info()
+        
+    def study_mode(self):
         print(f"{self.name} in online Program")
 
 class OfflineStudent(Student):
     def offline_info(self):
         self.display_info()
+        
+    def study_mode(self):
         print(f"{self.name} in offline Program")
 
         
 s1 = OnlineStudent("Tarun", 7 , "Ethical Hacking","10th")
 s2 = OfflineStudent("Ritu", 28 , "Full Stack","12th")
 
-s1.online_info()
-print("-" * 20)
-s2.offline_info()
+students = [s1,s2]
+
+for s in students:
+    s.display_info()
+    s.study_mode()
+    print("-" * 20)
